174994586ab6b9a0932d7abce5a1d0ab9cda049c
alexander kostuchenko <cat6654@gmail.com>
2014-12-16 13:48:31 +0200

    Fixing Farm randomness

--- src/main/java/Farm/App.java
+++ src/main/java/Farm/App.java
@@ -3,6 +3,7 @@ package Farm;
 import HomeWork2.CountChars;
 
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -11,11 +12,23 @@ import java.util.Scanner;
 public class App {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Specify Farm size.");
         Farm farm = new Farm(scanner.nextInt());
         Farmer farmer = new Farmer();
         farm.growPlants();
-        farmer.gatherPlants();
-
+        //farmer.gatherPlants();
+        Map<String, Integer> gatheredPlants = farmer.gatherPlants();
+        Random rand = new Random();
+        if(gatheredPlants.size() <1) {
+            System.out.println("Water plants more, man");
+            System.out.println("Farmer: watering...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            farmer.waterPlants(rand.nextInt(10));
+        }
         for(Map.Entry<String, Integer> element :farmer.gatherPlants().entrySet())
         {
             System.out.println(element.getKey() + ":" + element.getValue());
