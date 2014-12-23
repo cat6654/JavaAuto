7d0dd3cf7c6462b1ab5121ad61b0f4588d22c756
alexander kostuchenko <cat6654@gmail.com>
2014-12-23 14:56:59 +0200

    Adding Read config from xml

--- src/main/java/ALMREST/RestClient/Tests/AlmRestCRUDTest.java
+++ src/main/java/ALMREST/RestClient/Tests/AlmRestCRUDTest.java
@@ -1,8 +1,10 @@
 package ALMREST.RestClient.Tests;
 
 import ALMREST.RestClient.AlmRestClient;
+import ALMREST.RestClient.Utils.ConnectionProperties;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -15,30 +17,36 @@ import java.util.Random;
 public class AlmRestCRUDTest {
     Random rand = new Random();
 
-@Test
-public void TestGetAllDefects() throws Exception {
-    AlmRestClient client = new AlmRestClient();
-    //login
-    client.login("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/","sa","");
-    Map<String,String> responseGet = client.sendRequest(
-            "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects",
-            "GET",
-            "",
-            null);
-    System.out.println("Status Code: " + responseGet.get("StatusCode"));
-    System.out.println(responseGet.get("Body"));
-    //logout
-    client.logout("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/");
-    Assert.assertTrue("Status codes do not match. Expected is 200. Actual is " + responseGet.get("StatusCode"), responseGet.get("StatusCode").equals("200"));
-}
+    @BeforeClass
+    public static void LoadProperties() {
+        ConnectionProperties.ReadConfiguration();
+    }
+
+    @Test
+    public void TestGetAllDefects() throws Exception {
+        AlmRestClient client = new AlmRestClient();
+        //login
+        client.login(ConnectionProperties.getAlmRestBaseURL(), ConnectionProperties.getUser(), ConnectionProperties.getPassword());
+        Map<String, String> responseGet = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects",
+                "GET",
+                "",
+                null);
+        System.out.println("Status Code: " + responseGet.get("StatusCode"));
+        System.out.println(responseGet.get("Body"));
+        //logout
+        client.logout(ConnectionProperties.getAlmRestBaseURL());
+        Assert.assertTrue("Status codes do not match. Expected is 200. Actual is " + responseGet.get("StatusCode"), responseGet.get("StatusCode").equals("200"));
+    }
+
     @Test
     public void TestPostDefect() throws Exception {
         AlmRestClient client = new AlmRestClient();
-        Map<String,Object> postBody = new HashMap<String,Object>();
+        Map<String, Object> postBody = new HashMap<String, Object>();
         //login
-        client.login("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/","sa","");
-        Map<String,String> responsePost = client.sendRequest(
-                "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects",
+        client.login(ConnectionProperties.getAlmRestBaseURL(), ConnectionProperties.getUser(), ConnectionProperties.getPassword());
+        Map<String, String> responsePost = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects",
                 "POST",
                 "{\"detected-by\": \"sa\",\"creation-time\": \"2014-11-11\",\"severity\": \"3-High\",\"name\": \"Created from REST " + rand.nextInt() + "\"}",
                 null);
@@ -51,78 +59,78 @@ public void TestGetAllDefects() throws Exception {
     @Test
     public void TestGetDefect() throws Exception {
         AlmRestClient client = new AlmRestClient();
-        Map<String,Object> postBody = new HashMap<String,Object>();
+        Map<String, Object> postBody = new HashMap<String, Object>();
         //login
-        client.login("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/","sa","");
-        Map<String,String> response = client.sendRequest(
-                "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects",
+        client.login(ConnectionProperties.getAlmRestBaseURL(), ConnectionProperties.getUser(), ConnectionProperties.getPassword());
+        Map<String, String> response = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects",
                 "POST",
                 "{\"detected-by\": \"sa\",\"creation-time\": \"2014-11-11\",\"severity\": \"3-High\",\"name\": \"Created from REST " + rand.nextInt() + "\"}",
                 null);
         System.out.println("Status Code: " + response.get("StatusCode"));
         System.out.println(response.get("Body"));
         //get created defect id from response body
-        Map<String,Object> postResult = new ObjectMapper().readValue(response.get("Body"), postBody.getClass());
+        Map<String, Object> postResult = new ObjectMapper().readValue(response.get("Body"), postBody.getClass());
         String id = postResult.get("id").toString();
-        Map<String,String> responseGet = client.sendRequest(
-                "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects/" + id,
+        Map<String, String> responseGet = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects/" + id,
                 "GET",
                 "",
                 null);
         //logout
-        client.logout("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/");
+        client.logout(ConnectionProperties.getAlmRestBaseURL());
         Assert.assertTrue("Status codes do not match. Expected is 200. Actual is " + responseGet.get("StatusCode"), responseGet.get("StatusCode").equals("200"));
     }
 
     @Test
     public void TestDeleteDefect() throws Exception {
         AlmRestClient client = new AlmRestClient();
-        Map<String,Object> postBody = new HashMap<String,Object>();
+        Map<String, Object> postBody = new HashMap<String, Object>();
         //login
-        client.login("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/","sa","");
-        Map<String,String> response = client.sendRequest(
-                "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects",
+        client.login(ConnectionProperties.getAlmRestBaseURL(), ConnectionProperties.getUser(), ConnectionProperties.getPassword());
+        Map<String, String> response = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects",
                 "POST",
                 "{\"detected-by\": \"sa\",\"creation-time\": \"2014-11-11\",\"severity\": \"3-High\",\"name\": \"Created from REST " + rand.nextInt() + "\"}",
                 null);
         System.out.println("Status Code: " + response.get("StatusCode"));
         System.out.println(response.get("Body"));
         //get created defect id from response body
-        Map<String,Object> postResult = new ObjectMapper().readValue(response.get("Body"), postBody.getClass());
+        Map<String, Object> postResult = new ObjectMapper().readValue(response.get("Body"), postBody.getClass());
         String id = postResult.get("id").toString();
-        Map<String,String> responseDelete = client.sendRequest(
-                "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects/" + id,
+        Map<String, String> responseDelete = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects/" + id,
                 "DELETE",
                 "",
                 null);
         //logout
-        client.logout("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/");
+        client.logout(ConnectionProperties.getAlmRestBaseURL());
         Assert.assertTrue("Status codes do not match. Expected is 200. Actual is " + responseDelete.get("StatusCode"), responseDelete.get("StatusCode").equals("200"));
     }
 
     @Test
     public void TestUpdateDefect() throws Exception {
         AlmRestClient client = new AlmRestClient();
-        Map<String,Object> postBody = new HashMap<String,Object>();
+        Map<String, Object> postBody = new HashMap<String, Object>();
         //login
-        client.login("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/","sa","");
-        Map<String,String> response = client.sendRequest(
-                "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects",
+        client.login(ConnectionProperties.getAlmRestBaseURL(), ConnectionProperties.getUser(), ConnectionProperties.getPassword());
+        Map<String, String> response = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects",
                 "POST",
                 "{\"detected-by\": \"sa\",\"creation-time\": \"2014-11-11\",\"severity\": \"3-High\",\"name\": \"Created from REST " + rand.nextInt() + "\"}",
                 null);
         System.out.println("Status Code: " + response.get("StatusCode"));
         System.out.println(response.get("Body"));
         //get created defect id from response body
-        Map<String,Object> postResult = new ObjectMapper().readValue(response.get("Body"), postBody.getClass());
+        Map<String, Object> postResult = new ObjectMapper().readValue(response.get("Body"), postBody.getClass());
         String id = postResult.get("id").toString();
-        Map<String,String> responsePut = client.sendRequest(
-                "http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/domains/ALEX_K/projects/sql/defects/" + id,
+        Map<String, String> responsePut = client.sendRequest(
+                ConnectionProperties.getAlmRestDomainProject() + "/defects/" + id,
                 "PUT",
                 "{\"name\": \"Created from REST " + rand.nextInt() + "\"}",
                 null);
         //logout
-        client.logout("http://myd-vm04769.hpswlabs.adapps.hp.com:8080/qcbin/api/");
+        client.logout(ConnectionProperties.getAlmRestBaseURL());
         Assert.assertTrue("Status codes do not match. Expected is 200. Actual is " + responsePut.get("StatusCode"), responsePut.get("StatusCode").equals("200"));
     }
 
